a5c3971ee171af3a813196d9433f5a60ccecdb21
Vikki Scruton <vikki.scruton@gmail.com>
2020-04-28 11:20:43 +0100

    Started to Reimplement

---
+++ HolidayHomeAway/ViewModels/OrderManagementViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HolidayHomeAway.Models;
+
+namespace HolidayHomeAway.ViewModels
+{
+    public class OrderManagementViewModel
+    {
+        public IEnumerable<Order> Orders { get; set; }
+
+        public string CurrentType { get; set; }
+    }
+}
